1b2d1de5f03c42e5c030373f427cea4b046570bb
Joe Hartley <joejohnhartley@gmail.com>
2023-01-20 14:30:23 +0000

    longest word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,20 +4,28 @@ require "open-uri"
 
 class GamesController < ApplicationController
   def new
-    @letters = Array.new(10) { [*'A'..'Z'].sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
   end
 
   def score
-    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    user_serialized = URI.open(url).read
-    data = JSON.parse(user_serialized)
-    choices = params[:word].upcase.split(//) - params[:letters].split(//)
-    if choices.empty? && data["found"] == true
-      @score = "Great word!"
-    elsif data["found"] == false
-      @score = "Sorry, but #{params[:word]} does not seem to be a valid word"
+    if included?(params[:word].upcase, params[:letters].split)
+      if english_word?(params[:word])
+        @score = "Great word!"
+      else
+        @score = "Sorry, but #{params[:word]} does not seem to be a valid word"
+      end
     else
       @score = "Sorry, but #{params[:word]} can't be built out of #{params[:letters]}"
     end
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
